b7b3ce0c883cdc79cdf0c0c27fadfea3683de9ab
UTCorp <utatind@gmail.com>
2015-08-24 03:24:04 +0530

    UTC Home
    Creating the first web presence. Upload v00

---
+++ act.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+if($_POST["submit"]) {
+    $recipient="dpriyeshvk@live.co.uk";
+    $subject="RESPONSE VIA UT CORP";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
